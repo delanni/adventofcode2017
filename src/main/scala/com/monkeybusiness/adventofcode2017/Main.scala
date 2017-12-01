41820c3e1be28ddcbf579a625ba8406ef7319d9c
Alex Szabo <alex.szabo@klarna.com>
2017-12-01 17:40:55 +0100

    First challenge

---
+++ src/main/scala/com/monkeybusiness/adventofcode2017/Main.scala
@@ -0,0 +1,9 @@
+package com.monkeybusiness.adventofcode2017
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("-First-")
+    println(Challenge1.solveFirst(Challenge1.input))
+    println(Challenge1.solveSecond(Challenge1.input))
+  }
+}
